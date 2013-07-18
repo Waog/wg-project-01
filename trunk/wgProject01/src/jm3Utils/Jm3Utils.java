7a80f43dc471dec2ff790ab60d8466df685ff4a3
oliver.stadie@gmail.com <oliver.stadie@gmail.com@2372456a-7916-b458-b0bb-9a7a0edf54d0>
2013-07-18 19:35:25 +0000

    ADDED: JM3 utility package and class (for drawing debug lines)

---
+++ trunk/wgProject01/src/jm3Utils/Jm3Utils.java
@@ -0,0 +1,33 @@
+package jm3Utils;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.shape.Box;
+import com.jme3.scene.shape.Line;
+
+/**
+ * Provides static methods for operating on the jm3 framework.
+ * 
+ * @author oli
+ * 
+ */
+public class Jm3Utils {
+
+	/**
+	 * places a visible line in the coordinate system of the given node, between
+	 * the given end points.
+	 */
+	public static void drawLine(Vector3f start, Vector3f end, Node node, AssetManager assetManager) {
+		Line line = new Line(start, end);
+		Geometry geometry = new Geometry("Line", line);
+		Material mat = new Material(assetManager,
+				"Common/MatDefs/Misc/Unshaded.j3md");
+		mat.setColor("Color", ColorRGBA.White);
+		geometry.setMaterial(mat);
+		node.attachChild(geometry);
+	}
+}
---
+++ trunk/wgProject01/src/jm3Utils/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Utility methods and classes for the jm3 framework.
+ * @author oli
+ *
+ */
+package jm3Utils;
