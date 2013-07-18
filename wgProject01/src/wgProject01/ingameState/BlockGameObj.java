7382518dac7540711522bde48929bf6a0b815a27
oliver.stadie <oliver.stadie@gmail.com>
2013-07-18 18:16:29 +0000

    ADDED: continued implementing block management...

--- wgProject01/src/wgProject01/ingameState/BlockGameObj.java
+++ wgProject01/src/wgProject01/ingameState/BlockGameObj.java
@@ -2,4 +2,12 @@ package wgProject01.ingameState;
 
 public class BlockGameObj {
 
+	/**
+	 * Informs the block about a changed neighbor block in the current direction.
+	 * This block acts accordingly by turning on and of it's mathes.
+	 */
+	void doHandleNeighborChangeAt(int i, int j, int k, BlockGameObj newNeighbor) {
+		
+	}
+
 }
--- wgProject01/src/wgProject01/ingameState/BlockManager.java
+++ wgProject01/src/wgProject01/ingameState/BlockManager.java
@@ -16,31 +16,72 @@ import utils.BetterArray3D;
  * 
  */
 public class BlockManager {
-	
+
 	/**
 	 * The singleton instance of the block manager.
 	 */
 	private static BlockManager singletonInstance;
-	
+
 	/**
 	 * The 3 dimensional automatically scaling Array of blocks.
 	 */
-	BetterArray3D<BlockGameObj> blockArray = new BetterArray3D<BlockGameObj>();
-	
+	private BetterArray3D<BlockGameObj> blockArray = new BetterArray3D<BlockGameObj>();
+
 	/**
 	 * Returns the singleton instance of the block manager.
 	 */
-	public BlockManager getInstance() {
+	BlockManager getInstance() {
 		if (singletonInstance == null) {
 			singletonInstance = new BlockManager();
 		}
-		
+
 		return singletonInstance;
 	}
-	
+
 	/**
 	 * Initializes the block manager.
 	 */
 	private BlockManager() {
+	}
+
+	/**
+	 * Places the given block at the given coordinates. Overrides any block
+	 * which was possibly at the given position. informs all neighbor blocks
+	 * about the change.
+	 */
+	void setBlock(int x, int y, int z, BlockGameObj newBlock) {
+		blockArray.set(x, y, z, newBlock);
+
+		// inform the neighbors of the block about the change
+		// loop into all 3 dimmensions...
+		for (int xOffset = -1; xOffset <= 1; xOffset++) {
+			for (int yOffset = -1; yOffset <= 1; yOffset++) {
+				for (int zOffset = -1; zOffset <= 1; zOffset++) {
+					// ... check if the current iteration is a direct neighbor
+					// or a diagonal neighbor...
+					Boolean directNeighbor = ((Math.abs(xOffset)
+							+ Math.abs(yOffset) + Math.abs(zOffset)) == 1);
+					if (directNeighbor) {
+						// ... check if the neighbor exists ...
+						BlockGameObj curNeighbor = blockArray.get(x + xOffset,
+								y + yOffset, z + zOffset);
+						if (curNeighbor != null) {
+							// ... and inform it if so.
+							curNeighbor.doHandleNeighborChangeAt(-xOffset,
+									-yOffset, -zOffset, newBlock);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Returns the block at the given position or null if the given position is
+	 * empty.
+	 */
+	BlockGameObj getBlock(int x, int y, int z) {
+		return blockArray.get(x, y, z);
+
 	}
 }
