232e7ba64cde6a9e8957707626ca3864dc3526f6
oliver.stadie <oliver.stadie@gmail.com>
2013-07-18 18:41:48 +0000

    ADDED: finished block management, but did not test it yet...

--- wgProject01/src/wgProject01/ingameState/BlockManager.java
+++ wgProject01/src/wgProject01/ingameState/BlockManager.java
@@ -1,7 +1,7 @@
 package wgProject01.ingameState;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.jme3.asset.AssetManager;
+import com.jme3.scene.Node;
 
 import utils.BetterArray3D;
 
@@ -26,6 +26,17 @@ public class BlockManager {
 	 * The 3 dimensional automatically scaling Array of blocks.
 	 */
 	private BetterArray3D<BlockGameObj> blockArray = new BetterArray3D<BlockGameObj>();
+	
+	
+	/**
+	 * The Node to which all Block have to be attached.
+	 */
+	private Node node;
+	
+	/**
+	 * The asset manager.
+	 */
+	private AssetManager assetManager;
 
 	/**
 	 * Returns the singleton instance of the block manager.
@@ -45,14 +56,42 @@ public class BlockManager {
 	}
 
 	/**
+	 * Initializes the Block Manager with it's needed data.
+	 * 
+	 * @param blockNode
+	 *            the Node to which all blocks of the BlockManager shall be
+	 *            attached.
+	 */
+	void initData(Node blockNode, AssetManager assetManager) {
+		this.node = blockNode;
+		this.assetManager = assetManager;
+	}
+	
+	/**
+	 * Returns a new Block game object.
+	 */
+	BlockGameObj getBlockGameObj() {
+		return new BlockGameObj(this.node, this.assetManager);
+	}
+
+	/**
 	 * Places the given block at the given coordinates. Overrides any block
 	 * which was possibly at the given position. informs all neighbor blocks
 	 * about the change.
 	 */
 	void setBlock(int x, int y, int z, BlockGameObj newBlock) {
+		// inform the old block, that it's removed
+		BlockGameObj oldBlock = blockArray.get(x, y, z);
+		if (oldBlock != null) {
+			oldBlock.doHandleRemovementFrom();
+		}
+
+		// place the block and inform it, that it's placed
 		blockArray.set(x, y, z, newBlock);
+		newBlock.doHandlePlacementAt(x, y, z);
 
 		// inform the neighbors of the block about the change
+		// and the block about it's neighbors
 		// loop into all 3 dimmensions...
 		for (int xOffset = -1; xOffset <= 1; xOffset++) {
 			for (int yOffset = -1; yOffset <= 1; yOffset++) {
@@ -65,6 +104,9 @@ public class BlockManager {
 						// ... check if the neighbor exists ...
 						BlockGameObj curNeighbor = blockArray.get(x + xOffset,
 								y + yOffset, z + zOffset);
+						// ... inform the new block anyway about the neighbor...
+						newBlock.doHandleNeighborChangeAt(xOffset, yOffset,
+								zOffset, curNeighbor);
 						if (curNeighbor != null) {
 							// ... and inform it if so.
 							curNeighbor.doHandleNeighborChangeAt(-xOffset,
