4547d33201033996d7b9425bd43e09ac7d1cde81
oliver.stadie@gmail.com <oliver.stadie@gmail.com@2372456a-7916-b458-b0bb-9a7a0edf54d0>
2013-07-19 08:54:59 +0000

    Cleaned up and further testing. - improved the "sun" - fixed the transparency issue for the blocks - cleaned up codestructure and comments

--- trunk/wgProject01/src/jm3Utils/Jm3Utils.java
+++ trunk/wgProject01/src/jm3Utils/Jm3Utils.java
@@ -6,7 +6,6 @@ import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
-import com.jme3.scene.shape.Box;
 import com.jme3.scene.shape.Line;
 
 /**
