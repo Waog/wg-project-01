dde2dd057de196720f1ed2a06845875ae49932b5
oliver.stadie <oliver.stadie@gmail.com>
2013-07-23 14:17:30 +0000

    FIXED: cleaned up.

--- wgProject01/src/wgProject01/ingameState/control/package-info.java
+++ wgProject01/src/wgProject01/ingameState/control/package-info.java
@@ -1,8 +1,19 @@
 /**
+ * <p>
  * The MVC (model view controller) pattern is used to divide the game into smaller modules.
  * This is the control module, which contains the game logic. 
+ * </p>
  * 
- * It manipulates the model module and receives commands from the view module if user inputs occur.
+ * <p> 
+ * It manipulates the model module and receives commands from the view module if
+ * user inputs occur.
+ * </p>
+ * 
+ * <p>
+ * <strong>Internal coding convention:</strong>
+ * all class names of this package shall have a postfix <code>Ctrl</code>,
+ * for example <code>EnemyCtrl</code>. 	
+ * </p>
  * 
  * @author oli
  *
