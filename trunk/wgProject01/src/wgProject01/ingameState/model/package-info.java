74c8a57ce3b7a8023887c5ea9d0c90492baaf0cc
oliver.stadie@gmail.com <oliver.stadie@gmail.com@2372456a-7916-b458-b0bb-9a7a0edf54d0>
2013-07-23 14:17:30 +0000

    FIXED: cleaned up.

--- trunk/wgProject01/src/wgProject01/ingameState/model/package-info.java
+++ trunk/wgProject01/src/wgProject01/ingameState/model/package-info.java
@@ -1,7 +1,18 @@
 /**
+ * <p>
  * The MVC (model view controller) pattern is used to divide the game into smaller modules.
  * This is the model module, which contains the the game data.
+ * </p>
+ * 
+ * <p>
  * It's read by the view module and manipulated by the control module.
+ * </p>
+ * 
+ * <p>
+ * <strong>Internal coding convention:</strong>
+ * all class names of this package shall have a postfix <code>Model</code>,
+ * for example <code>EnemyModel</code>. 	
+ * </p>
  * 
  * @author oli
  *
--- trunk/wgProject01/src/wgProject01/ingameState/view/package-info.java
+++ trunk/wgProject01/src/wgProject01/ingameState/view/package-info.java
@@ -1,9 +1,19 @@
 /**
+ * <p>
  * The MVC (model view controller) pattern is used to divide the game into smaller modules.
  * This is the view module, which contains the the games visualization and user inputs.
+ * </p>
  * 
+ * <p>
  * It reads the model module to know what to visualize and
  * maps user inputs to actions of the control module. 
+ * </p>
+ * 
+ * <p>
+ * <strong>Internal coding convention:</strong>
+ * all class names of this package shall have a postfix <code>View</code>,
+ * for example <code>EnemyView</code>. 	
+ * </p>
  * 
  * @author oli
  *
