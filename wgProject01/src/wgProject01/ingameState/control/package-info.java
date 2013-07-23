d278e847809827efbe70612c8c5b9e7ae59c0e0a
oliver.stadie <oliver.stadie@gmail.com>
2013-07-23 14:05:17 +0000

    ADDED: MVC packages

---
+++ wgProject01/src/wgProject01/ingameState/control/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * The MVC (model view controller) pattern is used to divide the game into smaller modules.
+ * This is the control module, which contains the game logic. 
+ * 
+ * It manipulates the model module and receives commands from the view module if user inputs occur.
+ * 
+ * @author oli
+ *
+ */
+package wgProject01.ingameState.control;
