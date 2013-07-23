9de12676352e027064ead36065c785a8e05f7946
oliver.stadie@gmail.com <oliver.stadie@gmail.com@2372456a-7916-b458-b0bb-9a7a0edf54d0>
2013-07-23 21:04:22 +0000

    ADDED: first implementing classes of Artemis component system.

---
+++ trunk/wgProject01/src/wgProject01/ingameState/gameLogic/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains all the game logic, this means the non-visual part of the game.
+ * 
+ * @author oli
+ *
+ */
+package wgProject01.ingameState.gameLogic;
--- trunk/wgProject01/src/wgProject01/ingameState/model/EnemyComponent.java
+++ trunk/wgProject01/src/wgProject01/ingameState/model/EnemyComponent.java
@@ -6,5 +6,5 @@ import com.artemis.Component;
  * The model data of an enemy. Currently empty, it's main purpose is to differ
  * enemy entities from non-enemy entities.
  */
-public class EnemyModel extends Component {
+public class EnemyComponent extends Component {
 }
