57aac7dbd91bd58cbcca23c8ac9e4c65748410a8
oliver.stadie <oliver.stadie@gmail.com>
2013-07-23 17:43:30 +0000

    ADDED: first implementing classes of Artemis component system.

---
+++ wgProject01/src/wgProject01/ingameState/model/EnemyModel.java
@@ -0,0 +1,10 @@
+package wgProject01.ingameState.model;
+
+import com.artemis.Component;
+
+/**
+ * The model data of an enemy. Currently empty, it's main purpose is to differ
+ * enemy entities from non-enemy entities.
+ */
+public class EnemyModel extends Component {
+}
