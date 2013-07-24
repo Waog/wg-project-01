5915977a509006d567bb1d51cd01935c825f5c49
mirco_kraenz@yahoo.de <mirco_kraenz@yahoo.de@2372456a-7916-b458-b0bb-9a7a0edf54d0>
2013-07-24 16:04:00 +0000

    Implemented: showing the collision box in debug mode

--- trunk/wgProject01/src/wgProject01/ingameState/gameLogic/view/EntityView.java
+++ trunk/wgProject01/src/wgProject01/ingameState/gameLogic/view/EntityView.java
@@ -1,11 +1,21 @@
 package wgProject01.ingameState.gameLogic.view;
 
+import jm3Utils.Jme3Utils;
+import wgProject01.Settings;
+import wgProject01.ingameState.gameLogic.components.CollisionBoxComponent;
 import wgProject01.ingameState.gameLogic.components.PositionComponent;
 
 import com.artemis.Entity;
+import com.jme3.asset.AssetManager;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import com.jme3.scene.control.AbstractControl;
+import com.jme3.scene.shape.Box;
 
 /**
  * Wraps an (Artemis) Entity object and is attached to a visual model (a JME
@@ -21,11 +31,44 @@ public class EntityView extends AbstractControl {
 	 */
 	private Entity entity;
 
+	/** the AssetManager of the simpleApplication */
+	AssetManager assetManager;
+
+	/**
+	 * the Node containing mostly the entities, but now also the component box
+	 * of it
+	 */
+	Node entityNode;
+
+	private Geometry collisionBoxGeometry;
+
 	/**
 	 * Constructs a new View for the given entity.
 	 */
 	public EntityView(Entity entity) {
 		this.entity = entity;
+	}
+
+	/** initializes the entity viewers datafields */
+	public void init(AssetManager assetManager, Node entityNode) {
+		this.assetManager = assetManager;
+		this.entityNode = entityNode;
+
+		
+		// TODO 2 set the right debug mode
+		if (Settings.debugMode >= 2) {
+			PositionComponent positionComponent = entity
+					.getComponent(PositionComponent.class);
+
+			CollisionBoxComponent collisionBoxComponent = entity
+					.getComponent(CollisionBoxComponent.class);
+			if (collisionBoxComponent != null && positionComponent != null) {
+				collisionBoxGeometry = Jme3Utils.getCuboid(collisionBoxComponent.radii, assetManager);
+				collisionBoxGeometry.getMaterial().getAdditionalRenderState().setWireframe(true);
+				collisionBoxGeometry.setLocalTranslation(positionComponent.pos);
+				entityNode.attachChild(collisionBoxGeometry);
+			}
+		}
 	}
 
 	/**
@@ -45,6 +88,17 @@ public class EntityView extends AbstractControl {
 				.getComponent(PositionComponent.class);
 		if (positionComponent != null) {
 			spatial.setLocalTranslation(positionComponent.pos);
+		}
+
+		// TODO 2 set the right debug mode
+		if (Settings.debugMode >= 2) {
+			// show the collision box
+			CollisionBoxComponent collisionBoxComponent = entity
+					.getComponent(CollisionBoxComponent.class);
+			if (collisionBoxComponent != null && positionComponent != null && collisionBoxGeometry != null) {
+				collisionBoxGeometry.setLocalTranslation(positionComponent.pos);
+				entityNode.attachChild(collisionBoxGeometry);
+			}
 		}
 	}
 
