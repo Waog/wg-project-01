8c2561d37474140321a8de28611c6248ab5e039a
mirco_kraenz <mirco_kraenz@yahoo.de>
2013-07-25 21:51:45 +0000

    FINISHED: implementing general parts of InputHandler and InputHandlingSystem

--- wgProject01/src/wgProject01/ingameState/gameLogic/view/InputHandler.java
+++ wgProject01/src/wgProject01/ingameState/gameLogic/view/InputHandler.java
@@ -1,7 +1,6 @@
 package wgProject01.ingameState.gameLogic.view;
 
 import wgProject01.GameApplication;
-import wgProject01.ingameState.gameLogic.components.InputReactingComponent;
 import wgProject01.ingameState.gameLogic.systems.InputHandlingSystem;
 
 import com.jme3.app.Application;
@@ -13,6 +12,11 @@ import com.jme3.input.KeyInput;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
 
+/**
+ * 
+ * @author Mirco
+ *
+ */
 public class InputHandler extends AbstractAppState implements ActionListener {
 
 	/**
@@ -20,64 +24,35 @@ public class InputHandler extends AbstractAppState implements ActionListener {
 	 * {@link AssetManager} itself
 	 */
 	private InputManager inputManager;
-	private GameApplication app;
-	
-	/** names for actions */
-	/** for moving left */ 
-	private static final String LEFT = "Left";
-	/** for moving right */
-	private static final String RIGHT = "Right";
-	/** for moving down */
-	private static final String BACK = "Back";
-	/** for moving up */
-	private static final String FORWARD = "Forward";
-	
-	
+
 	@Override
 	public void initialize(AppStateManager stateManager, Application app) {
 		// initialize datafields
 		super.initialize(stateManager, app);
-		this.app = (GameApplication) app;
 		this.inputManager = app.getInputManager();
-		
+
 		initKeys();
 	}
-	
-	
-	
+
 	private void initKeys() {
-		inputManager.addMapping(LEFT, new KeyTrigger(KeyInput.KEY_A));
-		inputManager.addMapping(RIGHT, new KeyTrigger(KeyInput.KEY_D));
-		inputManager.addMapping(BACK, new KeyTrigger(KeyInput.KEY_S));
-		inputManager.addMapping(FORWARD, new KeyTrigger(KeyInput.KEY_W));
-		inputManager.addListener(this, LEFT);
-		inputManager.addListener(this, RIGHT);
-		inputManager.addListener(this, BACK);
-		inputManager.addListener(this, FORWARD);
-		
+		inputManager.addMapping(InputHandlingSystem.LEFT, new KeyTrigger(
+				KeyInput.KEY_A));
+		inputManager.addMapping(InputHandlingSystem.RIGHT, new KeyTrigger(
+				KeyInput.KEY_D));
+		inputManager.addMapping(InputHandlingSystem.BACK, new KeyTrigger(
+				KeyInput.KEY_S));
+		inputManager.addMapping(InputHandlingSystem.FORWARD, new KeyTrigger(
+				KeyInput.KEY_W));
+		inputManager.addListener(this, InputHandlingSystem.LEFT);
+		inputManager.addListener(this, InputHandlingSystem.RIGHT);
+		inputManager.addListener(this, InputHandlingSystem.BACK);
+		inputManager.addListener(this, InputHandlingSystem.FORWARD);
+
 	}
-
-
 
 	@Override
 	public void onAction(String name, boolean isPressed, float tpf) {
-		if(name.equals(LEFT)){
-			InputHandlingSystem.moveLeftActivated = isPressed;
-		}
-		if(name.equals(RIGHT)){
-			InputHandlingSystem.moveRightActivated = isPressed;
-		}
-		
-		if(name.equals(BACK)){
-			InputHandlingSystem.moveBackActivated = isPressed;
-		}
-		
-		if(name.equals(FORWARD)){
-			InputHandlingSystem.moveForwardActivated = isPressed;
-		}
-		
-		
+		InputHandlingSystem.mapper.put(name, isPressed);
 	}
 
-	
 }
