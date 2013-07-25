0f9dfb1f6a41c5679731b641f3786d246f82072b
mirco_kraenz@yahoo.de <mirco_kraenz@yahoo.de@2372456a-7916-b458-b0bb-9a7a0edf54d0>
2013-07-25 21:51:45 +0000

    FINISHED: implementing general parts of InputHandler and InputHandlingSystem

--- trunk/wgProject01/src/wgProject01/ingameState/gameLogic/systems/InputHandlingSystem.java
+++ trunk/wgProject01/src/wgProject01/ingameState/gameLogic/systems/InputHandlingSystem.java
@@ -1,5 +1,8 @@
 package wgProject01.ingameState.gameLogic.systems;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import wgProject01.ingameState.gameLogic.components.InputReactingComponent;
 import wgProject01.ingameState.gameLogic.components.PositionComponent;
 
@@ -8,7 +11,6 @@ import com.artemis.ComponentMapper;
 import com.artemis.Entity;
 import com.artemis.annotations.Mapper;
 import com.artemis.systems.EntityProcessingSystem;
-import com.jme3.math.Vector3f;
 
 /**
  * An entity system handling the given user inputs. The inputs commands are
@@ -18,16 +20,20 @@ import com.jme3.math.Vector3f;
  * 
  */
 public class InputHandlingSystem extends EntityProcessingSystem {
+	
+	/** names for commands */
+	/** for moving left */ 
+	public static final String LEFT = "Left";
+	/** for moving right */
+	public static final String RIGHT = "Right";
+	/** for moving down */
+	public static final String BACK = "Back";
+	/** for moving up */
+	public static final String FORWARD = "Forward";
 
-	/** flag marking if entity shall move to left, set by external class */
-	public static boolean moveLeftActivated;
-	/** flag marking if entity shall move to right, set by external class */
-	public static boolean moveRightActivated;
-	/** flag marking if entity shall move to down, set by external class */
-	public static boolean moveBackActivated;
-	/** flag marking if entity shall move to up, set by external class */
-	public static boolean moveForwardActivated;
-
+	/** the map mapping the keys given as Strings into boolean values */
+	public static Map<String, Boolean> mapper = new HashMap<String, Boolean>();;
+	
 	/**
 	 * Automagical creation of a ComponentMapper to extract a component from the
 	 * entities.
@@ -58,19 +64,25 @@ public class InputHandlingSystem extends EntityProcessingSystem {
 		PositionComponent positionComponent = positionManager.get(e);
 		InputReactingComponent inputReactingComponent = inputReactingManager
 				.get(e);
-		if (moveLeftActivated) {
+		if(getMappedValue(LEFT)){
 			positionComponent.pos.x -= inputReactingComponent.speed * delta;
 		}
-		if(moveRightActivated){
+		if(getMappedValue(RIGHT)){
 			positionComponent.pos.x += inputReactingComponent.speed * delta;
 		}
-		if(moveBackActivated){
+		if(getMappedValue(BACK)){
 			positionComponent.pos.z -= inputReactingComponent.speed * delta;
 		}
-		if(moveForwardActivated){
+		if(getMappedValue(FORWARD)){
 			positionComponent.pos.z += inputReactingComponent.speed * delta;
 		}
 		
-
+	}
+	
+	/**
+	 * returns the mapped value for the given key from the mapper variable or false if the given key is not defined 
+	 */
+	private boolean getMappedValue(String key){
+		return (mapper.get(key) != null && mapper.get(key));
 	}
 }
