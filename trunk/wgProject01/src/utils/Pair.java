73af93313f623d3de12342eb3b7dd04498ce2516
oliver.stadie@gmail.com <oliver.stadie@gmail.com@2372456a-7916-b458-b0bb-9a7a0edf54d0>
2013-07-27 19:26:24 +0000

    Generated hash code and equals methods

--- trunk/wgProject01/src/utils/Pair.java
+++ trunk/wgProject01/src/utils/Pair.java
@@ -9,4 +9,41 @@ public class Pair<A, B> {
 		this.first = first;
 		this.second = second;
 	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((first == null) ? 0 : first.hashCode());
+		result = prime * result + ((second == null) ? 0 : second.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pair other = (Pair) obj;
+		if (first == null) {
+			if (other.first != null)
+				return false;
+		} else if (!first.equals(other.first))
+			return false;
+		if (second == null) {
+			if (other.second != null)
+				return false;
+		} else if (!second.equals(other.second))
+			return false;
+		return true;
+	}
 }
