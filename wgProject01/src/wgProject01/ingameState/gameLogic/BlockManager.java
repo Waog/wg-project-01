b6340b1b2aaef087f36cb6247bd1ba7558132386
oliver.stadie <oliver.stadie@gmail.com>
2013-07-27 19:26:24 +0000

    Generated hash code and equals methods

--- wgProject01/src/wgProject01/ingameState/gameLogic/BlockManager.java
+++ wgProject01/src/wgProject01/ingameState/gameLogic/BlockManager.java
@@ -1,5 +1,6 @@
 package wgProject01.ingameState.gameLogic;
 
+import com.bulletphysics.collision.dispatch.PairCachingGhostObject;
 import com.jme3.asset.AssetManager;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Node;
