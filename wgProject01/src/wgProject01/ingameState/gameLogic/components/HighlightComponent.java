5f162e0dca348e0231bc1ab9aeaa1274ce5b70e8
oliver.stadie <oliver.stadie@gmail.com>
2013-07-29 22:52:19 +0000

    FIXED: cleaned up.

---
+++ wgProject01/src/wgProject01/ingameState/gameLogic/components/HighlightComponent.java
@@ -0,0 +1,13 @@
+package wgProject01.ingameState.gameLogic.components;
+
+import com.artemis.Component;
+
+/**
+ * A {@link Component} (pure data structure) to mark an entity as block face
+ * highlight.
+ * 
+ * @author oli
+ * 
+ */
+public class HighlightComponent extends Component {
+}
