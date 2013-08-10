cb74be82181d55981388575f56383b21da939ec6
oliver.stadie@gmail.com <oliver.stadie@gmail.com@2372456a-7916-b458-b0bb-9a7a0edf54d0>
2013-08-10 20:23:00 +0000

    ADDED: implementation of observer pattern for nifty handling

---
+++ trunk/wgProject01/src/utils/typeModels/SimpleModel.java
@@ -0,0 +1,24 @@
+package utils.typeModels;
+
+import java.util.Observable;
+
+/**
+ * See {@link utils.typeModels package description} on how to use this class.
+ */
+public class SimpleModel<T> extends Observable {
+	private T internal;
+
+	public SimpleModel(T initialValue) {
+		super();
+		this.internal = initialValue;
+	}
+
+	public T get() {
+		return internal;
+	}
+
+	public void set(T internal) {
+		this.internal = internal;
+		super.notifyObservers();
+	}
+}
